4271a47603841bffb10c6be133f716e712576523
tim <tim@sy-edm.com>
2016-03-10 17:46:57 +0400

    Signals updating List

--- calc.py
+++ calc.py
@@ -4,16 +4,58 @@ from calcui import Ui_Form
 import threading
 import time
 
-
 class Communicate(QtWidgets.QWidget):
-    closeApp = QtCore.pyqtSignal() 
+    """
+    Small Class based on a Widget
+    It's only purpose is to contain a signal object
+    """
+    sig = QtCore.pyqtSignal()
 
 class calc(Ui_Form):
+    """
+    MycCalc implementation
+    This inherits from the ClassUI which was created in Qt Designer
+    """
 
     def __init__(self,*argv,**argvw):
-       self.c=Communicate() 
+        """
+        Initialize the Calc class, creating a Communicate object
+        :param argv:
+        :param argvw:
+        :return:
+        """
+        self.c=Communicate()
+
+    def setupUi(self,*argv,**argvw):
+        """
+        Setup the "wiring" in the GUI
+        This should not add new widgets to the UI.
+        We connect widgets to functions
+        and signals to slots.
+
+        It is important to call the base method also
+
+        :param argv:
+        :param argvw:
+        :return:
+        """
+        super(calc,self).setupUi(*argv,**argvw)
+        self.N1.setText("123")
+        self.N2.setText("333")
+        self.btnCalc.clicked.connect(self.chk_fun)
+        self.btnProcess.clicked.connect(self.start_worker)
+        self.btnSignal.clicked.connect(self.sendSignal)
+        self.c.sig.connect(self.hi_slot)
 
     def chk_fun(self):
+        """
+        This is called from a Straight Button
+        It it connected up via
+
+        self.btnCalc.clicked.connect(self.chk_fun)
+
+        :return:
+        """
         a=int(self.N1.toPlainText())
         b=int(self.N2.toPlainText())
         self.RESULT.setText(str(a+b))
@@ -21,32 +63,50 @@ class calc(Ui_Form):
 
 
     def sendSignal(self):
-        self.c.closeApp.emit()
+        """
+        This sends the signal !!
+        :return:
+        """
+        self.c.sig.emit()
 
-   
-    def worker(self):                  # start another thread
-        for i in range(10):
-            print("Worker %d"%i)
-            time.sleep(1) 
 
- 
     def start_worker(self):
+        """
+        This is called from a Button - the wiring is
+
+        self.btnProcess.clicked.connect(self.start_worker)
+
+        This creates a thread and runs the method worker
+
+        :return:
+        """
         print("Process")
         threading.Thread(target=self.worker, name="_proc").start()
 
+
+    def worker(self):
+        """
+        This is the code that the worker thread will run
+
+        :return:
+        """
+        for i in range(10):
+            print("Worker %d"%i)
+            time.sleep(1)
+
     def hi_slot(self):
+        """
+        A Slot which will receive the signal
+        the signal is sent from a button -
+
+        This is wired by
+        self.c.sig.connect(self.hi_slot)
+
+        :return:
+        """
         print("hi signal received")
         item = QListWidgetItem("Hi Slot")
         self.lvSignal.addItem(item)
-
-    def setupUi(self,*argv,**argvw):
-        super(calc,self).setupUi(*argv,**argvw)
-        self.N1.setText("123")
-        self.N2.setText("333")
-        self.btnCalc.clicked.connect(self.chk_fun)
-        self.btnProcess.clicked.connect(self.start_worker)
-        self.btnSignal.clicked.connect(self.sendSignal)
-        self.c.closeApp.connect(self.hi_slot) 
 
 if __name__ == "__main__":
     import sys
