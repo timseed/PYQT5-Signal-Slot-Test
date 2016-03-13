bbc19b6acbda8ab4675c3e637fa067995a9972b0
tim <tim@sy-edm.com>
2016-03-13 19:20:50 +0400

    Working

--- calc.py
+++ calc.py
@@ -1,15 +1,33 @@
-from PyQt5 import QtCore, QtGui, QtWidgets 
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import *
 from calcui import Ui_Form
-import threading
 import time
 
-class Communicate(QtWidgets.QWidget):
+
+class Communicate(QtCore.QObject):
     """
     Small Class based on a Widget
     It's only purpose is to contain a signal object
+    Note: Type QObject
     """
-    sig = QtCore.pyqtSignal()
+    sig = QtCore.pyqtSignal(object)
+
+class qworker(QThread):
+
+    def __init__(self,*argv,**argvw):
+        super(qworker, self).__init__(*argv,**argvw)
+        print("qworker init")
+        self.c=Communicate()
+
+
+    def run(self):
+        for i in range(4):
+            print("Worker %d"%i)
+            self.c.sig.emit("Worker "+str(i))
+            time.sleep(1)
+        self.c.sig.emit("Worker Done")
+        print("Worker done")
 
 class calc(Ui_Form):
     """
@@ -24,7 +42,10 @@ class calc(Ui_Form):
         :param argvw:
         :return:
         """
+
+        super(self.__class__, self).__init__()
         self.c=Communicate()
+        #self.workerthread=QThread()
 
     def setupUi(self,*argv,**argvw):
         """
@@ -61,40 +82,47 @@ class calc(Ui_Form):
         self.RESULT.setText(str(a+b))
         print("chk fun")
 
-
     def sendSignal(self):
         """
         This sends the signal !!
+        Needs a Parameter now...
         :return:
         """
-        self.c.sig.emit()
-
+        self.c.sig.emit("Button")
 
     def start_worker(self):
         """
         This is called from a Button - the wiring is
-
-        self.btnProcess.clicked.connect(self.start_worker)
-
-        This creates a thread and runs the method worker
-
+        This creates a qthread and runs the method worker
+        The worker has been connected to a slot here - so when the worker emit the main gui is updated
         :return:
         """
         print("Process")
-        threading.Thread(target=self.worker, name="_proc").start()
+        #Create Class based on QThread
+        self.worker=qworker()
+        # Next we need to connect the events from that thread to functions we want
+        # to be run when those signals get fired
+        self.worker.c.sig.connect(self.hi_slot)
+        #start links to qworker.run method
+        self.worker.start()
 
-
-    def worker(self):
+    def Non_SIGNAL_worker(self):
         """
         This is the code that the worker thread will run
+        However this code can not send SIGNALS - or more accuratly
+        SIGNALS sent by this code will not go to a slot
+
+        This code currently not used
 
         :return:
         """
+
+        workerC=Communicate()
         for i in range(10):
             print("Worker %d"%i)
             time.sleep(1)
 
-    def hi_slot(self):
+    def hi_slot(self,data):
         """
         A Slot which will receive the signal
         the signal is sent from a button -
@@ -105,8 +133,10 @@ class calc(Ui_Form):
         :return:
         """
         print("hi signal received")
-        item = QListWidgetItem("Hi Slot")
+        item = QListWidgetItem(data)
         self.lvSignal.addItem(item)
+
+
 
 if __name__ == "__main__":
     import sys
